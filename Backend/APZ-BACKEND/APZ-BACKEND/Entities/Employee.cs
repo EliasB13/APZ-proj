e0f7fc711d6f2837cf8d9b1388ef08b0167a88c2
Elias <etrofunenko@gmail.com>
2019-11-01 17:21:47 +0200

    Created backend application, added database migrations.

---
+++ Backend/APZ-BACKEND/APZ-BACKEND/Entities/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APZ_BACKEND.Entities
+{
+	public class Employee
+	{
+		public int Id { get; set; }
+
+		public PrivateUser PrivateUser { get; set; }
+		public BusinessUser BusinessUser { get; set; }
+		public EmployeesRole EmployeesRole { get; set; }
+	}
+}
---
+++ Backend/APZ-BACKEND/APZ-BACKEND/Entities/SharedItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APZ_BACKEND.Entities
+{
+	public class SharedItem
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public string Description { get; set; }
+		public byte[] RfidNumber { get; set; }
+
+		public BusinessUser BusinessUser { get; set; }
+		public IEnumerable<ItemTakingLine> ItemTakingLines { get; set; }
+		public IEnumerable<EmployeeRoleItem> EmployeeRoleItems { get; set; }
+	}
+}
