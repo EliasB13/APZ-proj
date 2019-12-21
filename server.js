5eb9d413cc500f6d6e4c8d3358f67d04682183fd
Elias <etrofunenko@gmail.com>
2019-12-21 15:52:30 +0200

    Fix.

--- server.js
+++ server.js
@@ -6,14 +6,8 @@ const app = express();
 
 app.use("/", serveStatic(path.join(__dirname, "/dist")));
 
-// app.get(/.*/, function(req, res) {
-//   res.sendFile(path.join(__dirname, "/dist/index.html"));
-// });
-
-app.get("/.well-known/acme-challenge/:content", function(req, res) {
-  res.send(
-    "FpOUZ1hmB4Hp4zLOsnPHLu5ucs00q5OhUiu4NZEL73g.TGzRBueBvD3MzUTuoJsBpvnHxOLf8K3Um0pM57Ca26g"
-  );
+app.get(/.*/, function(req, res) {
+  res.sendFile(path.join(__dirname, "/dist/index.html"));
 });
 
 const port = process.env.PORT || 8080;
