db592f591b9770fdf3cfe01d385ae2f8cac7113a
Elias <etrofunenko@gmail.com>
2019-12-21 15:46:30 +0200

    Update

--- server.js
+++ server.js
@@ -6,12 +6,14 @@ const app = express();
 
 app.use("/", serveStatic(path.join(__dirname, "/dist")));
 
-app.get(/.*/, function(req, res) {
-  res.sendFile(path.join(__dirname, "/dist/index.html"));
-});
+// app.get(/.*/, function(req, res) {
+//   res.sendFile(path.join(__dirname, "/dist/index.html"));
+// });
 
 app.get("/.well-known/acme-challenge/:content", function(req, res) {
-  res.send("xxxxxxxxxxxx-yyyy.zzzzzzzzzzzzzzzzzzz");
+  res.send(
+    "FpOUZ1hmB4Hp4zLOsnPHLu5ucs00q5OhUiu4NZEL73g.TGzRBueBvD3MzUTuoJsBpvnHxOLf8K3Um0pM57Ca26g"
+  );
 });
 
 const port = process.env.PORT || 8080;
