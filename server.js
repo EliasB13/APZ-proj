498d576dc0f6807f96c79423ee234021724b924f
Elias <etrofunenko@gmail.com>
2019-12-21 15:41:35 +0200

    Trying to beat acme-challenge.

--- server.js
+++ server.js
@@ -10,6 +10,10 @@ app.get(/.*/, function(req, res) {
   res.sendFile(path.join(__dirname, "/dist/index.html"));
 });
 
+app.get("/.well-known/acme-challenge/:content", function(req, res) {
+  res.send("xxxxxxxxxxxx-yyyy.zzzzzzzzzzzzzzzzzzz");
+});
+
 const port = process.env.PORT || 8080;
 app.listen(port);
 console.log(`app is listening on port: ${port}`);
